01395e8b0f6681b3a4359a57d2b1770d8c56dc47
sunzhichao <sunzhichao@chehejia.com>
2017-11-14 10:17:19 +0800

    初始化文件

---
+++ src/test/java/com/supreme/demo/TestConfig.java
@@ -0,0 +1,9 @@
+package com.supreme.demo;
+
+/**
+ * Created by dustin on 2017/3/19.
+ */
+public class TestConfig {
+    //TODO use YOUR chatbot webhook
+    public static final String CHATBOT_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=11445093d86b99fa2362730a086322a1a957a7e87f0e08deb16d56963d980d66";
+}
